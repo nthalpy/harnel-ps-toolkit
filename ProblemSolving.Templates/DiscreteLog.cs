c65024dd7eb3c44bbbafb6a3b417270eda8164b8
Harnel <harnel.tngn@gmail.com>
2024-06-21 14:33:56 +0900

    separate dlp

---
+++ ProblemSolving.Templates/DiscreteLog.cs
@@ -0,0 +1,46 @@
+﻿using ProblemSolving.Templates.Merger;
+using System;
+using System.Collections.Generic;
+
+namespace ProblemSolving.Templates
+{
+    [IncludeIfReferenced]
+    public static class DiscreteLog
+    {
+        /// <summary>
+        /// Find b, such that b^p = x
+        /// </summary>
+        public static bool BabyStepGiantStep(long b, long x, long mod, Dictionary<long, long> baby, out long p)
+        {
+            baby.Clear();
+
+            var step = (long)Math.Sqrt(mod);
+
+            var babypow = 1L;
+            for (var idx = 0; idx < step; idx++)
+            {
+                if (baby.ContainsKey(babypow))
+                    break;
+
+                baby[babypow] = idx;
+                babypow = babypow * b % mod;
+            }
+
+            var g = NumberTheory.FastPow(b, (mod - 2) * step, mod);
+            var giantpow = x;
+            for (var idx = 0; idx < step + 2; idx++)
+            {
+                if (baby.TryGetValue(giantpow, out var smolidx))
+                {
+                    p = smolidx + idx * step;
+                    return true;
+                }
+
+                giantpow = giantpow * g % mod;
+            }
+
+            p = default;
+            return false;
+        }
+    }
+}
--- ProblemSolving.Templates/NumberTheory.cs
+++ ProblemSolving.Templates/NumberTheory.cs
@@ -1,6 +1,5 @@
 ﻿using ProblemSolving.Templates.Merger;
 using System;
-using System.Collections.Generic;
 
 namespace ProblemSolving.Templates
 {
@@ -29,42 +28,6 @@ namespace ProblemSolving.Templates
             }
 
             return rv;
-        }
-
-        /// <summary>
-        /// Find b, such that b^p = x
-        /// </summary>
-        public static bool BabyStepGiantStep(long b, long x, long mod, Dictionary<long, long> baby, out long p)
-        {
-            baby.Clear();
-
-            var step = (long)Math.Sqrt(mod);
-
-            var babypow = 1L;
-            for (var idx = 0; idx < step; idx++)
-            {
-                if (baby.ContainsKey(babypow))
-                    break;
-
-                baby[babypow] = idx;
-                babypow = babypow * b % mod;
-            }
-
-            var g = FastPow(b, (mod - 2) * step, mod);
-            var giantpow = x;
-            for (var idx = 0; idx < step + 2; idx++)
-            {
-                if (baby.TryGetValue(giantpow, out var smolidx))
-                {
-                    p = smolidx + idx * step;
-                    return true;
-                }
-
-                giantpow = giantpow * g % mod;
-            }
-
-            p = default;
-            return false;
         }
     }
 }
