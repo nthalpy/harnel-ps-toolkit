4af641e00db399c254d052ceb7702bd526c74c48
Harnel <harnel.tngn@gmail.com>
2024-07-15 20:37:46 +0900

    add mod sum seg

---
+++ ProblemSolving.Templates/SegmentTree/Impl/ModSumSeg.cs
@@ -0,0 +1,18 @@
+﻿namespace ProblemSolving.Templates.SegmentTree.Impl
+{
+    [IncludeIfReferenced]
+    public sealed class ModSumSeg : AbelianGroupSegTree<long, long, long>
+    {
+        private int _mod;
+
+        public ModSumSeg(int size, int mod) : base(size)
+        {
+            _mod = mod;
+        }
+
+        protected override long ApplyDiff(long element, long diff) => (element + diff) % _mod;
+        protected override long CreateDiff(long element, long val) => (val + _mod - element) % _mod;
+        protected override long Identity() => 0;
+        protected override long Merge(long l, long r) => (l + r) % _mod;
+    }
+}
