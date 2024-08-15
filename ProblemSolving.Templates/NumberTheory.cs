b8939e3dc089d64a815f91e4228a343a65c814cd
Harnel <harnel.tngn@gmail.com>
2024-08-15 22:04:46 +0900

    add IntervalTree`1, Factorization, GetDivisors

--- ProblemSolving.Templates/NumberTheory.cs
+++ ProblemSolving.Templates/NumberTheory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProblemSolving.Templates
 {
@@ -27,6 +28,62 @@ namespace ProblemSolving.Templates
             }
 
             return rv;
+        }
+
+        public static List<(long prime, int count)> Factorize(long v)
+        {
+            var factors = new List<(long prime, int count)>();
+
+            if (v % 2 == 0)
+            {
+                var rank2 = 0;
+                while (v % 2 == 0)
+                {
+                    v /= 2;
+                    rank2++;
+                }
+
+                factors.Add((2, rank2));
+            }
+
+            for (var p = 3L; p * p <= v; p += 2)
+                if (v % p == 0)
+                {
+                    var rank = 0;
+                    while (v % p == 0)
+                    {
+                        v /= p;
+                        rank++;
+                    }
+
+                    factors.Add((p, rank));
+                }
+
+            if (v != 1)
+                factors.Add((v, 1));
+
+            return factors;
+        }
+
+        public static List<long> GetDivisors(List<(long prime, int count)> factors)
+        {
+            var divisors = new List<long>();
+            divisors.Add(1);
+
+            foreach (var (prime, count) in factors)
+            {
+                var divcount = divisors.Count;
+                var ppow = 1L;
+
+                for (var pow = 1; pow <= count; pow++)
+                {
+                    ppow *= prime;
+                    for (var idx = 0; idx < divcount; idx++)
+                        divisors.Add(divisors[idx] * ppow);
+                }
+            }
+
+            return divisors;
         }
     }
 }
--- ProblemSolving.Templates/Treaps/IntervalTree.cs
+++ ProblemSolving.Templates/Treaps/IntervalTree.cs
@@ -39,4 +39,43 @@ namespace ProblemSolving.Templates.Treaps
             return false;
         }
     }
+
+    [IncludeIfReferenced]
+    public class IntervalTree<T> : Treap<(long StIncl, long EdExcl, T Value)>
+        where T : struct
+    {
+        public IntervalTree()
+            : base(0)
+        {
+        }
+
+        public bool FindRangeFromPoint(long point, out long stIncl, out long edExcl, out T val)
+        {
+            (stIncl, edExcl, val) = (default, default, default);
+
+            var curr = _root;
+            while (curr != null)
+            {
+                if (curr.Key.StIncl <= point && point < curr.Key.EdExcl)
+                {
+                    (stIncl, edExcl, val) = curr.Key;
+                    return true;
+                }
+                else if (curr.Key.EdExcl <= point)
+                {
+                    curr = curr.Right;
+                }
+                else if (point < curr.Key.StIncl)
+                {
+                    curr = curr.Left;
+                }
+                else
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+
+            return false;
+        }
+    }
 }
